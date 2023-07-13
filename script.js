4c0d0ea36af9f4055b5cbe4746cb6b4865845a84
Shubham Kumar <110241594+Shubhama0@users.noreply.github.com>
2023-07-13 18:59:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+async function f1(){
+    document.querySelector('#one').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#one').style.display = "none";
+
+    }, 4000);
+}
+
+async function f2(){
+    document.querySelector('#two').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#two').style.display="none";
+ 
+    }, 4000);
+}
+
+async function f3(){
+    document.querySelector('#three').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#three').style.display="none";
+ 
+    }, 4000);
+}
+
+async function f4(){
+    document.querySelector('#four').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#four').style.display="none";
+ 
+    }, 4000);
+}
+
+async function f5(){
+    document.querySelector('#five').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#five').style.display="none";
+ 
+    }, 4000);
+}
+
+async function f6(){
+    document.querySelector('#six').style.display="block";
+
+    setInterval(() => {
+        document.querySelector('#six').style.display="none";
+ 
+    }, 4000);
+}
